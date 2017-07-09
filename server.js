27d7f964824684ea62b6da1edbf588882fd607b3
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-07-09 15:50:00 +0700

    import express

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var chalk = require('chalk');
+var express = require('express');
 var config = require('./config.js');
 var global = require('./global.js');
 
+var app = express();
+
 MongoClient.connect(config.db.url + config.db.database, function (err, db) {
     assert.equal(null, err, '[ERROR] Cannot connect to ' + config.db.database);
-    console.log(chalk.bgGreen('Connect Successfully'));
+    console.log(chalk.bgGreen('[SUCESS] Connect to ' + config.db.database + ' successful'));
     db.close();
 });

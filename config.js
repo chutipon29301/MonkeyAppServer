8545c6660df755eed2cf4c868956fb9c9c2412a3
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-07-09 14:37:42 +0700

    Test connection to mongo

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    runPort: 3000,
+    debugMode: false,
+    db: {
+        url: 'mongodb://localhost:27017/',
+        database: 'monkeyDB',
+    }
+}
--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var config = require('./config.js');
+var chalk = require('chalk');
+
+MongoClient.connect(config.db.url + config.db.database, function (err, db) {
+    assert.equal(null, err, '[ERROR] Cannot connect to ' + config.db.database);
+    console.log(chalk.bgGreen("Connect Successfully"));
+    db.close();
+});
